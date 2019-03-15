6a5fa43ece8418f3cca3384f8f04e47890a77bb3
ValiantGinger <david.silady@gmail.com>
2019-03-15 22:08:47 +0100

    first layouts

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../view/loginStage/login.fxml"));
+        primaryStage.setTitle("SEKS | Login");
+        primaryStage.setScene(new Scene(root, 1136, 640));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
