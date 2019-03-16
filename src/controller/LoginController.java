576bc8550b09636f5736f78c8cf126dfc74bfa14
ValiantGinger <david.silady@gmail.com>
2019-03-16 17:22:08 +0100

    first layouts

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
+	@FXML
+	private ResourceBundle resources;
+	
+	@FXML
+	private URL location;
+	
+	@FXML
+	private JFXTextField loginUsername;
+	
+	@FXML
+	private JFXButton loginSignUpButton;
+	
+	@FXML
+	private JFXButton loginButton;
+	
+	@FXML
+	private JFXPasswordField loginPassword;
+	
+	@FXML
+	void initialize() {
+		loginButton.setOnAction(event -> {
+			System.out.println("Login button clicked!");
+		});
+	}
 }
