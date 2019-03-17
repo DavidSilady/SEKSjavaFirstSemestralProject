142a93f7e7fe73d9659deb0fa529445f834fdedb
ValiantGinger <david.silady@gmail.com>
2019-03-17 14:19:46 +0100

    first layouts

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -4,12 +4,24 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController {
+	@FXML
+	private AnchorPane content;
+	
 	@FXML
 	private ResourceBundle resources;
 	
@@ -28,10 +40,32 @@ public class LoginController {
 	@FXML
 	private JFXPasswordField loginPassword;
 	
+	
+	
 	@FXML
-	void initialize() {
+	void initialize()  throws Exception {
 		loginButton.setOnAction(event -> {
 			System.out.println("Login button clicked!");
 		});
+	}
+	
+	
+	public void loadChooseSignUp (javafx.event.ActionEvent actionEvent) throws Exception{
+		/*FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/signUpCompany.fxml"));
+		Parent root1 = (Parent) fxmlLoader.load();
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.setTitle("Sign Up Company | SEKS");
+		stage.setScene(new Scene(root1));
+		stage.show();*/
+		
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/signUpCompany.fxml"));
+		Parent signUpParent = loader.load();
+		Scene signUpScene = new Scene(signUpParent);
+		Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+		window.setScene(signUpScene);
+		window.show();
 	}
 }
--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class SignUpController {
+import com.jfoenix.controls.JFXButton;
 
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class SignUpController {
+	
+	@FXML
+	private ResourceBundle resources;
+	
+	@FXML
+	private URL location;
+	
+	@FXML
+	private JFXButton signUpLoginButton;
+	
+	@FXML
+	private JFXButton signUpButton;
+	
+	
+	@FXML
+	void initialize() {
+	
+	}
 }
