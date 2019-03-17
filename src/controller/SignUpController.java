f1ceb7672fe342f2a1c6636c95fe3498b1aa925f
ValiantGinger <david.silady@gmail.com>
2019-03-17 19:32:10 +0100

    first layouts

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -2,10 +2,16 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SignUpController {
 	
@@ -25,5 +31,17 @@ public class SignUpController {
 	@FXML
 	void initialize() {
 	
+	}
+	
+	
+	public void loadLoginScene (ActionEvent actionEvent) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		// Redirect to Sign Up scene (load the targeted .fxml scene)
+		loader.setLocation(getClass().getResource("/view/login.fxml"));
+		Parent newParent = loader.load();
+		Scene newScene = new Scene(newParent);
+		Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+		window.setScene(newScene);
+		window.show();
 	}
 }
