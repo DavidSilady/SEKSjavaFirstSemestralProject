222c6c24a0637bdc9c1103bb7d55ee34fdb403bc
DavidSilady <david.silady@gmail.com>
2019-04-08 18:37:11 +0200

    adding company menu for auditor user

--- src/controller/CompanyMenuController.java
+++ src/controller/CompanyMenuController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
 public class CompanyMenuController {
+	@FXML
+	private AnchorPane dynamicPane;
+	
+	@FXML void initialize() throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/deviceMenu.fxml"));
+		Pane deviceListTable = (Pane) fxmlLoader.load();
+		try {
+			dynamicPane.getChildren().clear();
+			dynamicPane.getChildren().add(deviceListTable);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void companyListButton (ActionEvent actionEvent) {
+	}
+	
+	public void viewDeviceButton (ActionEvent actionEvent) {
+	}
+	
+	
+	
 }
