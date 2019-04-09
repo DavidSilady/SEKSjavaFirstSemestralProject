3e6680ba702050521850c1684290cab2175f1a1e
DavidSilady <david.silady@gmail.com>
2019-04-09 04:25:16 +0200

    notification bar

--- src/controller/NotificationController.java
+++ src/controller/NotificationController.java
@@ -1,9 +1,32 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.notification.Notification;
 
+import java.awt.event.ActionEvent;
+
 public class NotificationController {
-	public void popUpNotification (Notification notification) {
+	private Notification notification = null;
 	
+	public void popUpNotification (Notification notification) throws Exception{
+		setNotification(notification);
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/notificationPopUp.fxml"));
+		Parent root1 = (Parent) fxmlLoader.load();
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		//stage.initStyle(StageStyle.UNDECORATED);
+		stage.setTitle("Notification");
+		stage.setScene(new Scene(root1, 300, 180));
+		stage.show();
+	}
+	
+	public void setNotification(Notification notification) {
+		this.notification = notification;
 	}
 }
